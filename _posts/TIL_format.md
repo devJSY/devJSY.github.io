0f686b6b6b64fbeab2022d9d81bf5e6ef5a11cb1
Seungyeon Jung <wjdtmd486@naver.com>
2022-03-22 21:18:02 +0900

    Add TIL

---
+++ _posts/TIL_format.md
@@ -0,0 +1,28 @@
+---
+published: true
+title:  ""
+excerpt: ""
+
+categories:
+  - BOJ
+tags:
+  - [TIL, BOJ, Python]
+
+toc: true
+toc_sticky: true
+ 
+date: 2022-00-00
+last_modified_at: 2022-00-00
+---
+
+# **2022-00-00 TIL**
+
+# 🤔 오늘의 학습목표
+
+
+# 📃 학습내용
+
+# 😊 배우게 된 점
+
+
+# 📌참조링크
--- _posts/practice.py
+++ _posts/practice.py
@@ -103,26 +103,99 @@
         
 # print(result)
 
-#문제7 문자열 재정렬
-data = input()
-result = []
-value = 0
+# #문제7 문자열 재정렬
+# data = input()
+# result = []
+# value = 0
 
-#문자를 하나씩 확인하여
-for x in data:
-    # 알파벳인 경우 결과 리스트에 삽입
-    if x.isalpha():
-        result.append(x)
-    # 숫자는 따로 더하기
-    else:
-        value += int(x)
+# #문자를 하나씩 확인하여
+# for x in data:
+#     # 알파벳인 경우 결과 리스트에 삽입
+#     if x.isalpha():
+#         result.append(x)
+#     # 숫자는 따로 더하기
+#     else:
+#         value += int(x)
 
-# 알파벳을 오름차 순으로 정렬
-result.sort()
+# # 알파벳을 오름차 순으로 정렬
+# result.sort()
 
-#숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
-if value != 0:
-    result.append(str(value))
+# #숫자가 하나라도 존재하는 경우 가장 뒤에 삽입
+# if value != 0:
+#     result.append(str(value))
 
-# 최종 결과 출력(리스트를 문자열로 변환하여 출력)
-print("".join(result))
+# # 최종 결과 출력(리스트를 문자열로 변환하여 출력)
+# print("".join(result))
+
+# #스택 구현 예제 
+# stack = []
+
+# stack.append(5)
+# stack.append(2)
+# stack.append(3)
+# stack.append(7)
+# stack.pop()
+# stack.append(1)
+# stack.append(4)
+# stack.pop()
+
+# print(stack[::-1])
+# print(stack)
+
+# #큐 구현 예제
+# from collections import deque
+
+# # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+# queue = deque()
+
+# queue.append(5)
+# queue.append(2)
+# queue.append(3)
+# queue.append(7)
+# queue.popleft()
+# queue.append(1)
+# queue.append(4)
+# queue.popleft()
+
+# print(queue)
+# queue.reverse()
+# print(queue)
+
+# def recursive_funtion(i):
+#     # 100번째 호출을 했을 때 종료되도록 종료 조건 명시
+#     if i == 100:
+#         return
+#     print(i, '번째 재귀함수에서', i + 1, '번째 재귀함수를 호출합니다.')
+#     recursive_funtion(i + 1)
+#     print(i, '번째 재귀함수를 종료합니다.')
+
+# recursive_funtion(1)
+
+# # DFS 함수 정의
+# def dfs(graph, v, visited):
+#     # 현재 노드를 방문 처리
+#     visited[v] = True
+#     print(v, end=' ')
+#     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+#     for i in graph[v]:
+#         if not visited[i]:
+#             dfs(graph, i, visited)
+
+# # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+# graph = [
+#   [],
+#   [2, 3, 8],
+#   [1, 7],
+#   [1, 4, 5],
+#   [3, 5],
+#   [3, 4],
+#   [7],
+#   [2, 6, 8],
+#   [1, 7]
+# ]
+
+# # 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+# visited = [False] * 9
+
+# # 정의된 DFS 함수 호출
+# dfs(graph, 1, visited)
